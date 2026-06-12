2ff55c772bf57636632e8e424edec27c28d91ea6
JiaLiangChen99 <1356617750@qq.com>
2026-06-12 11:47:53 +0800

    change workdlow in apk

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+'''
+Author: JiaLiangChen99 1356617750@qq.com
+Date: 2026-06-12 11:30:17
+LastEditors: JiaLiangChen99 1356617750@qq.com
+LastEditTime: 2026-06-12 11:43:57
+FilePath: \qtale\main.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import flet as ft
 
 
