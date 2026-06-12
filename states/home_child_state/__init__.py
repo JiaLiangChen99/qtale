058bcdae6ed382fee17ec11a5f960ed38e53fbc7
JiaLiangChen99 <1356617750@qq.com>
2026-06-12 11:14:56 +0800

    delete not use file

--- states/home_child_state/__init__.py
+++
@@ -1,6 +0,0 @@
-from .find_state import FindState
-from .manager_state import ManagerState
-from .home_state import HomeState
-
-
-__all__ = ["FindState", "ManagerState", "HomeState"]
--- states/home_child_state/find_state.py
+++
@@ -1,5 +0,0 @@
-from fletx import Xstate
-
-class FindState(Xstate):
-    def __init__(self, page):
-        super().__init__(page)
